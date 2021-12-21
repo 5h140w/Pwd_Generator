364c787cf77df8edd0e52f17cdde6b8a2f05309d
5h140w <5h140w23@gmail.com>
2021-12-21 23:45:00 +0100

    v1

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-
+"""
 def intValidation(str):
     while True:
         try:
@@ -19,8 +19,8 @@ length= intValidation("Password Length: ")
 while length<5:
     print("Min Length is 5")
     length= intValidation("Password Length: ")
+"""
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
-
 def generate_password(length):
     random.shuffle(characters)
     password=""
@@ -28,4 +28,3 @@ def generate_password(length):
         password+=random.choice(characters)
     return password
 
-print(generate_password(length))
---
+++ server.py
@@ -0,0 +1,4 @@
+from password_generator import generate_password
+
+if __name__=="__main__":
+    print(generate_password(5))
