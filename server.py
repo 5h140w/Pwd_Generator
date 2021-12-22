ec76575153f79b1796c1e1d06cd504ed1870b19c
5h140w <5h140w23@gmail.com>
2021-12-23 00:14:47 +0100

    v1

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 from functions.password_generator import generate_password
 from functions.pwd import PWD
+from functions.password_generator import intValidation
 import sys
-
 
 if __name__=="__main__":
     if(len(sys.argv)==1):
         print(generate_password())
     else:
-        print(PWD(int(sys.argv[1])))
+        try:
+            x = int(sys.argv[1])
+        except ValueError:
+            print("this is not a integer")
+            print(generate_password())
+        else:
+            if (x<5):
+                print("Min Length is 5")
+                print(generate_password())
+            else:
+                print(PWD(x))
